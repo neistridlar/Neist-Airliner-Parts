ce4592325f82a61462fed89e2cd08bd190fce7ac
neistridlar <38767958+neistridlar@users.noreply.github.com>
2018-07-09 18:34:38 +0200

    31PC+31PD+TCS
    started work on 31Passenger cabing and 31 passenger door, as wel as Tail connector system.

---
+++ Neist Air - Airliner Parts/Assets/SeatConfigGenerator.py
@@ -0,0 +1,14 @@
+abrest = 6
+rows = 5
+seatnames = 'ABCDEFGHIJKLMNOPQRSTUVW'
+for i in range(abrest*rows):
+    seatname = str(1 + i//abrest) + seatnames[i % abrest]
+    print('	MODULE\n\
+		{\n\
+		name = InternalSeat\n\
+		seatTransformName = SeatTransform (' +
+		str(i+1)  + ')2\n\
+		kerbalEyeOffset = 0, 0.02, 0\n\
+		displayseatName =' + seatname + '\n\
+		allowCrewHelmet = false\n\
+		}')
